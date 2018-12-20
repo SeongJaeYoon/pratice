b1c1026aaec5b8bca390117dd7e35e3a843e7050
-m <-m>
2018-12-20 18:05:53 +0900

    YEAH

---
+++ Project2/Project2/MainScene.cpp
@@ -0,0 +1,20 @@
+#include "MainScene.h"
+
+
+MainScene::MainScene()
+{
+}
+
+
+MainScene::~MainScene()
+{
+}
+
+
+void MainScene::Update(float eTime){
+	ZeroIScene::Update(eTime);
+}
+
+void MainScene::Render(){
+	ZeroIScene::Render();
+}
---
+++ Project2/Project2/MainScene.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "C:\Users\Sunrin\source\repos\Project2\Project2\Include\ZeroIScene.h"
+class MainScene :
+	public ZeroIScene
+{
+public:
+	MainScene();
+	~MainScene();
+	void Update(float eTime);
+	void Render();
+};
+
